f37d3f368387b6dcb72cc791de6016c8a24b0098
Robert Gühne <robert@polyxostudios.de>
2020-04-26 01:16:45 +0200

    feat(Axios): interceptor added

---
+++ src/axiosInterceptors.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export default {
+  setupInterceptors: (store) => {
+    axios.interceptors.request.use(
+      (config) => {
+        const state = store.getState();
+        const token =
+          state.auth && state.auth.token ? state.auth.token : undefined;
+        if (token) {
+          config.headers["authorization"] = "JWT " + token;
+        }
+        return config;
+      },
+      (error) => {
+        Promise.reject(error);
+      }
+    );
+
+    axios.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      function (error) {
+        //catches if the session ended!
+        if (error.response.status === 401) {
+          localStorage.clear();
+          store.dispatch({ type: "LOGOUT", data: true });
+        }
+        return Promise.reject(error);
+      }
+    );
+  },
+};
--- src/decs.d.ts
+++
@@ -1,1 +0,0 @@
-declare module "@wfp/ui";
