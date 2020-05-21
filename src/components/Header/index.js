df7b64de6d8fa2ab9e46b4117d3aa8f153f19bb4
David Christian Jones <david@dcj.codes>
2020-05-21 10:32:59 -0400

    remove dead code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Link } from 'react-router-dom';
-
 import classNames from 'classnames';
 
 import Navigation from 'components/Navigation';
