481174fc450d5a46548d01d158c6d6c1aa11b6b7
David Christian Jones <david@dcj.codes>
2020-05-21 10:32:48 -0400

    add crucial styles to all stories

--- .storybook/decorators/provider.js
+++ .storybook/decorators/provider.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router';
+import '../../src/scss/global.scss';
+import '../../src/scss/ui.scss';
 
 const ProviderDecorator = storyFn => {
   return <MemoryRouter>{storyFn()}</MemoryRouter>;
