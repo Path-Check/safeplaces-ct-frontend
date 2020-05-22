8b64d4396513af0cc475ef0d5bee1958ec8f2c04
David Christian Jones <david@dcj.codes>
2020-05-22 15:48:02 -0400

    Button mods for review

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import styles from './styles.module.scss';
+import { button, buttonLarge, buttonSecondary } from './styles.module.scss';
 
-const Button = ({ text, width, height, disabled = false, onClick }) => {
-  return (
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+
+const Button = ({
+  text,
+  width,
+  height,
+  disabled = false,
+  onClick,
+  large,
+  secondary,
+  to,
+}) => {
+  const btnClasses = classNames({
+    [`${button}`]: true,
+    [`${buttonLarge}`]: large,
+    [`${buttonSecondary}`]: secondary,
+  });
+
+  return to ? (
+    <Link className={btnClasses} to={to} style={{ width, height }}>
+      Create Record Manually
+    </Link>
+  ) : (
     <button
-      className={styles.button}
+      className={btnClasses}
       style={{ width, height }}
       disabled={disabled}
       type="button"
