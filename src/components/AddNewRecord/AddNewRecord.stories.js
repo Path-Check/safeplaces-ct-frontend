bec02048b0a1ce7c80a27e1a615370d0356e62c9
David Christian Jones <david@dcj.codes>
2020-05-22 15:49:06 -0400

    Add New Record modal

---
+++ src/components/AddNewRecord/AddNewRecord.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import AddNewRecord from 'components/AddNewRecord';
+
+export default { title: 'Add New Record' };
+
+export const Default = () => <AddNewRecord currentRecord="123456" />;
---
+++ src/components/AddNewRecord/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Link } from 'react-router-dom';
+import Modal from 'components/Modal';
+import Dialog from 'components/Dialog';
+
+import {
+  AddNewRecordHeader,
+  AddNewRecordCode,
+  AddNewRecordActions,
+  AddNewRecordTitle,
+} from './AddNewRecord.module.scss';
+
+import Button from 'components/Button';
+
+const AddNewRecord = ({ currentRecord }) => {
+  return (
+    <Modal>
+      <Dialog width="650px">
+        <header className={AddNewRecordHeader}>
+          <h3 className={AddNewRecordTitle}>Add New Record</h3>
+          <p>
+            Share the access code below with a patient in order to load their
+            location data for contact tracing.
+          </p>
+        </header>
+        <p className={AddNewRecordCode}>869869</p>
+        <div>or</div>
+        <div className={AddNewRecordActions}>
+          {currentRecord && (
+            <Button large to={currentRecord} text="Create Record Manually" />
+          )}
+          <Button
+            secondary
+            large
+            onClick={() => console.log('Delete Case')}
+            text="Cancel"
+          />
+        </div>
+      </Dialog>
+    </Modal>
+  );
+};
+
+AddNewRecord.propTypes = {
+  currentRecord: PropTypes.string,
+};
+
+export default AddNewRecord;
