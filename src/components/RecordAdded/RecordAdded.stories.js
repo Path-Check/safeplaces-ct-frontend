82f7d8727849dc09defb6122677ec035164651af
David Christian Jones <david@dcj.codes>
2020-05-23 09:35:44 -0400

    Record Added UI

---
+++ src/components/RecordAdded/RecordAdded.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import RecordAdded from 'components/RecordAdded';
+
+export default { title: 'Record Added' };
+
+export const Default = () => <RecordAdded currentRecord="532523423" />;
