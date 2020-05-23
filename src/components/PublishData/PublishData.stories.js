c7d1e21b5e59e9d26462bdde353aba36f1fef37d
David Christian Jones <david@dcj.codes>
2020-05-23 11:23:13 -0400

    Publish Data UI

---
+++ src/components/PublishData/PublishData.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PublishData from 'components/PublishData';
+
+export default { title: 'Publish Data' };
+
+export const Default = () => (
+  <PublishData dataPoints="7,700" subscriberCount="2,355" />
+);
