ba85aef23d8964859febb8d9adfa0e7b8d7b4c5f
David Christian Jones <david@dcj.codes>
2020-05-23 13:01:15 -0400

    Initial development

---
+++ src/components/RedactorTools/Header/redactorToolsHeader.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import RedactorToolsHeader from 'components/RedactorTools/Header';
+
+export default { title: 'Redactor Tools Header' };
+
+export const Default = () => <RedactorToolsHeader currentRecord="532523423" />;
