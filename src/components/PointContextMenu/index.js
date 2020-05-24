e023cb55432648eab5c1f468be6504947095f404
David Christian Jones <david@dcj.codes>
2020-05-24 17:01:58 -0400

    Introduce context menu

---
+++ src/components/PointContextMenu/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import {
+  pointContextMenu,
+  pointContextMenuClose,
+} from './PointContextMenu.module.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faEdit,
+  faMinusCircle,
+  faTrash,
+  faTimes,
+} from '@fortawesome/pro-solid-svg-icons';
+
+const PointContextMenu = ({
+  id,
+  closeAction,
+  deleteAction,
+  editAction,
+  deselectAction,
+}) => {
+  return (
+    <div className={pointContextMenu}>
+      <button
+        className={pointContextMenuClose}
+        type="button"
+        onClick={closeAction}
+      >
+        <FontAwesomeIcon icon={faTimes} />
+      </button>
+      <ul>
+        <li>
+          <button type="button" onClick={() => deleteAction(id)}>
+            <FontAwesomeIcon icon={faEdit} />
+            Edit
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={() => editAction(id)}>
+            <FontAwesomeIcon icon={faMinusCircle} />
+            Unselect
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={() => deselectAction(id)}>
+            <FontAwesomeIcon icon={faTrash} />
+            Delete
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+PointContextMenu.propTypes = {
+  deleteAction: PropTypes.func,
+  editAction: PropTypes.func,
+  deselectAction: PropTypes.func,
+};
+
+export default PointContextMenu;
