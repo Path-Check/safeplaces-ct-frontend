ca80cc2ce2ed864e6240dcd4389e22f3835a2342
David Christian Jones <david@dcj.codes>
2020-05-24 14:29:16 -0400

    import existing work

---
+++ src/components/RedactorTools/FilterData/index.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  faCaretUp,
+  faCaretDown,
+  faFilter,
+} from '@fortawesome/pro-solid-svg-icons';
+
+import {
+  filterData,
+  filterDataHeader,
+  filterBody,
+  filterBodyActive,
+  filterBodyAction,
+} from './FilterData.module.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const FilterData = ({ children }) => {
+  const [filtersVisible, setFiltersVisible] = useState(false);
+
+  return (
+    <div className={filterData}>
+      <div className={filterDataHeader}>
+        <h5>
+          <FontAwesomeIcon icon={faFilter} /> Filter Data
+        </h5>
+        <button
+          className={filterBodyAction}
+          onClick={() => setFiltersVisible(!filtersVisible)}
+        >
+          {!filtersVisible ? 'Show' : 'Hide'} Filters
+          <FontAwesomeIcon icon={!filtersVisible ? faCaretDown : faCaretUp} />
+        </button>
+      </div>
+      <div
+        className={
+          filtersVisible ? `${filterBody} ${filterBodyActive}` : filterBody
+        }
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+FilterData.propTypes = {};
+
+export default FilterData;
