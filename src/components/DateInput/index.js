380b1ee09d773dd57d0cd0daf316bfe047658fbe
David Christian Jones <david@dcj.codes>
2020-05-25 09:42:46 -0400

    date picker fighting

--- src/components/DateInput/index.js
+++ src/components/DateInput/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { TextInput } from '@wfp/ui';
+import { SingleDatePickerInput } from '@wfp/ui';
 import styles from './styles.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarAlt, faClock } from '@fortawesome/pro-regular-svg-icons';
 import moment from 'moment';
 
+import 'react-dates/initialize';
+import { SingleDatePicker, DateRangePicker } from 'react-dates';
+
 export default function DateInput(props) {
-  const { min, max, time } = props;
-  // Format date value
-  // const value = moment(props.value).format("YYYY-MM-DD");
   return (
-    <TextInput
-      additional={
-        <div className={styles.icon}>
-          <FontAwesomeIcon icon={faCalendarAlt} />
-        </div>
-      }
-      type="date"
-      {...props}
-      formItemClassName={styles.dateInput}
-      placeholder=""
-      defaultValue=""
-    />
+    <div className={styles.dateInput}>
+      <SingleDatePickerInput
+        style={{ width: '100%' }}
+        datePicker={SingleDatePicker}
+        customInputIcon={
+          <FontAwesomeIcon className={styles.icon} icon={faCalendarAlt} />
+        }
+        type="date"
+        placeholder=""
+        defaultValue=""
+      />
+    </div>
   );
 }
