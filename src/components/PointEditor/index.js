82a0ec627c0a1215e9b7655f300656690af35526
David Christian Jones <david@dcj.codes>
2020-05-25 08:57:33 -0400

    Point Editor initial build

--- src/components/PointEditor/index.js
+++ src/components/PointEditor/index.js
@@ -2,25 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Button from 'components/Button';
+import DateInput from 'components/DateInput';
+import TimeInput from 'components/TimeInput';
+import TextInput from '@wfp/ui/lib/components/TextInput';
+import DurationInput from 'components/DurationInput';
+import LocationSearchInput from 'components/EntryForm/autoComplete';
+
+import {
+  pointEditor,
+  locationControls,
+  pointEditorHeader,
+  closeAction,
+  timeControls,
+} from './PointEditor.module.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCrosshairs, faTimes } from '@fortawesome/pro-solid-svg-icons';
 
 const PointEditor = ({ location, date, time, duration, type = 'add' }) => {
   return (
-    <div>
-      <h5>{type === 'add' ? 'Add Data to Record' : 'Edit Data'}</h5>
-      <div>
-        <div>Places control</div>
-        <span>or</span>
-        <div>Select from Map</div>
+    <div className={pointEditor}>
+      <div className={pointEditorHeader}>
+        <h4>{type === 'add' ? 'Add Data to Record' : 'Edit Data'}</h4>
+        <button className={closeAction}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
       </div>
-      <div>Date control</div>
-      <div>
-        <div>time control</div>
-        <div>duration control</div>
+      <div className={locationControls}>
+        {/* <LocationSearchInput /> */}
+        <span>or</span>
+        <Button text="Select from Map">
+          <FontAwesomeIcon icon={faCrosshairs} />
+        </Button>
+      </div>
+
+      <DateInput />
+      <div className={timeControls}>
+        <TimeInput time type="time" />
+        <DurationInput name="duration" />
       </div>
       <Button
         onClick={() => console.log('submit form')}
         type="submit"
-        text="Save Data"
+        text={`Save Data`}
       />
     </div>
   );
