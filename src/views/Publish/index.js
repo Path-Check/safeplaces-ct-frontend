471dc9796bd2d2774b450edfbaf1f2c1685e463e
David Christian Jones <david@dcj.codes>
2020-05-25 22:37:53 -0400

    Flesh out publish screen

--- src/views/Publish/index.js
+++ src/views/Publish/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
-const Publish = () => {
-  return <>Publish</>;
+import { publish } from './Publish.module.scss';
+
+import Map from 'components/_shared/Map';
+import RedactorTools from 'components/_shared/RedactorTools';
+import SidebarWrapper from 'components/_shared/Sidebar/SidebarWrapper';
+import SidebarHeader from 'components/_shared/Sidebar/SidebarHeader';
+import PublishToolActions from 'views/Publish/Actions/ToolActions';
+import PublishLoadActions from 'views/Publish/Actions/LoadActions';
+
+const Publish = ({ record }) => {
+  return (
+    <div class={publish}>
+      <SidebarWrapper>
+        {record ? (
+          <>
+            <RedactorTools /> <PublishToolActions />
+          </>
+        ) : (
+          <>
+            <SidebarHeader
+              title="Publish Data"
+              copy="Review and edit patient location data before publishing to your health authority subscribers."
+            />
+            <PublishLoadActions />
+          </>
+        )}
+      </SidebarWrapper>
+      <Map />
+    </div>
+  );
 };
 
 export default Publish;
