79a5d4efabd6845b896717d2de143a8ee283a450
David Christian Jones <david@dcj.codes>
2020-05-26 15:57:19 -0400

    check for record before rendering

--- src/views/Trace/Actions/ToolActions/index.js
+++ src/views/Trace/Actions/ToolActions/index.js
@@ -3,11 +3,15 @@ import PropTypes from 'prop-types';
 
 import { sidebarActions } from '../TracerActions.module.scss';
 import Button from 'components/_shared/Button';
+import { useSelector } from 'react-redux';
+import recordsSelectors from 'ducks/record/selectors';
 
 const TracerToolActions = () => {
+  const record = useSelector(state => recordsSelectors.getRecord(state));
+
   return (
     <div className={sidebarActions}>
-      <Button>Stage for Publishing</Button>
+      <Button disabled={!record?.points}>Stage for Publishing</Button>
       <Button secondary>Save Session</Button>
     </div>
   );
