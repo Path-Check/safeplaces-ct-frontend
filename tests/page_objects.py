cc755840956fc22d995064d9266ff0a0f6d7e4d1
Lynn Gazis <52673158+LynnGazis@users.noreply.github.com>
2020-05-26 17:57:57 -0700

    Update page_objects.py
    Add elements that can be used later to add the tests:
    1) Add an entry
    2)  Select an individual data point and edit it
    3) Select an individual data point and delete it
    
    CSS_SELECTOR values for the Individual Data Point class currently assume only one individual data point, and may need modification for the case where there is a table of multiple individual data points.

--- tests/page_objects.py
+++ tests/page_objects.py
@@ -74,6 +74,10 @@ class RedactionPage(Page):
     date_start = (By.ID, 'date-start')
     date_end = (By.ID, 'date-end')
     duration = (By.ID, 'duration')
+    add_entry_btn = (By.CSS_SELECTOR, '#root > div > div > div.styles_sidebar__28L4X > div.styles_toolbar__3nYxc > a > button')
+    delete_inspected_btn = (By.CSS_SELECTOR, '#root > div > div > div.styles_sidebar__28L4X > div.styles_toolbar__3nYxc > button:nth-child(2)')
+    select_all_btn = (By.CSS_SELECTOR, '#root > div > div > div.styles_sidebar__28L4X > div.styles_toolbar__3nYxc > button:nth-child(3) > svg')
+    select_none_btn = (By.CSS_SELECTOR, '#root > div > div > div.styles_sidebar__28L4X > div.styles_toolbar__3nYxc > button:nth-child(4)')
 
     def load_file(self, filename):
         self.find_element(self.load_file_btn).send_keys(filename)
@@ -89,6 +93,82 @@ class RedactionPage(Page):
 
     def check_duration_is(self, value):
         self.check_is(value,self.find_element(self.duration).text)
+        
+    def add_entry(self):
+        self.find_element(self.add_entry_btn).click()
+
+    def delete_inspected(self):
+        self.find_element(self.delete_inspected_btn).click()
+        
+    def select_all(self):
+        self.find_element(self.select_all_btn).click()
+        
+    def select_none(self):
+        self.find_element(self.select_none_btn).click()
+
+class NewPointEditor:
+    dateInput = (By.NAME, 'date')
+    timeInput = (By.NAME, 'time')
+    searchAddress = (By.CSS_SELECTOR, '#root > div > div > div.styles_editor__1yYbN > form > div.styles_content__1vUjG > div:nth-child(2) > div > input')
+    latitude = (By.NAME, 'latitude')
+    longitude = (By.NAME, 'longitude')
+    street = (By.NAME, 'street')
+    other = (By.NAME, 'other')
+    town = (By.NAME, 'town')
+    postalCode = (By.NAME, 'postal')
+    comment = (By.NAME, 'comment')
+    add_tracks_btn = (By.CSS_SELECTOR, '#root > div > div > div.styles_editor__1yYbN > form > div.styles_content__1vUjG > button')
+    pick_location_btn = (By.CSS_SELECTOR, '#root > div > div > div.styles_editor__1yYbN > form > div.styles_content__1vUjG > div.styles_position__2ql7M > span:nth-child(3) > button > svg')
+    get_coordinates_btn = (By.CSS_SELECTOR, '#root > div > div > div.styles_editor__1yYbN > form > div.styles_content__1vUjG > div.styles_position__2ql7M > span:nth-child(4) > button > svg')
+    
+    def enter_date(self, date):
+        self.find_element(self.dateInput).send_keys(date)
+
+    def enter_date(self, date):
+        self.find_element(self.dateInput).send_keys(date)
+
+    def enter_address(self, address):
+        self.find_element(self.searchAddress).send_keys(address)
+
+    def enter_latitude(self, latitude):
+        self.find_element(self.timeInput).send_keys(latitude)
+
+    def enter_longitude(self, date):
+        self.find_element(self.timeInput).send_keys(longitude)
+
+    def enter_street(self, street):
+        self.find_element(self.timeInput).send_keys(street)
+
+    def enter_other(self, date):
+        self.find_element(self.timeInput).send_keys(other)
+
+    def enter_town(self, date):
+        self.find_element(self.timeInput).send_keys(town)
+
+    def enter_postal_code(self, date):
+        self.find_element(self.timeInput).send_keys(postal_code)
+
+    def enter_comment(self, date):
+        self.find_element(self.timeInput).send_keys(comment)
+
+    def pick_location(self):
+        self.find_element(pick_location_btn).click()
+
+    def get_coordinates(self):
+        self.find_element(get_coordinates_btn).click()
+
+    def add_tracks(self):
+        self.find_element(add_tracks_btn).click()
+
+class IndividualDataPoint:
+    entry_selectbox = '#root > div > div > div.styles_sidebar__28L4X > div.styles_sidebarContent__3-5j0 > div > div.wfp--form-item.wfp--checkbox-wrapper.styles_checkbox__3IQ4N > label'
+    entry_editbox = '#root > div > div > div.styles_sidebar__28L4X > div.styles_sidebarContent__3-5j0 > div > div.styles_itemInner__3xvEO > div.styles_buttons__1phBJ > button:nth-child(1) > svg'
+    
+    def select_entry(self):
+        self.find_element(entry_selectbox).click()
+
+    def edit_entry(self):
+        self.find_element(entry_editbox).click()
 
 class Tools:
     def compare_files(self, fname1, fname2):
@@ -114,3 +194,4 @@ class Tools:
 
         f1.close()
         f2.close()
+
