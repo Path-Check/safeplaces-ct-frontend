a5c1ce2e01ceb43e4da19bf4da7b73c154f66e6d
FacundoMalgieri <facundo.malgieri@gmail.com>
2020-05-28 17:22:33 -0300

    partially replaced mocked user from login

--- src/ducks/auth/service.js
+++ src/ducks/auth/service.js
@@ -9,20 +9,19 @@ const authService = {
       url: `${REACT_APP_API_URL}login`,
       data,
     });
-    /* TODO
-    this will be used when the mock endpoint exists
-    if(token) {
-      const idRes = await axios({
-        method: 'POST',
+    const { token } = tokenRes.data;
+    let orgRes = null;
+    if (token) {
+      orgRes = await axios({
+        method: 'GET',
         url: `${REACT_APP_API_URL}organization`,
         data,
       });
     }
-    */
-    return {
-      user: { id: 1, name: 'HA Name', completedOnboarding: true },
-      token: tokenRes.data.token,
-    };
+    // TODO completedOnboarding will soon come from BE
+    const user = orgRes ? { ...orgRes.data, completedOnboarding: true } : null;
+
+    return { user, token };
   },
   onboarding: data => {
     return axios({
