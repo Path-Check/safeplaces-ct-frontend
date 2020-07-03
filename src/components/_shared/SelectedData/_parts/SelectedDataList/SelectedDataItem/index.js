b2352d7f3d0337fe79800635e3a5b8a6f169fa4d
Facundo Malgieri <facundo.malgieri@gmail.com>
2020-07-03 11:34:19 -0300

    [PLACES-457] Group Data by Date (#250)
    * [PLACES-457] Group data by date
    
    * wip

--- src/components/_shared/SelectedData/_parts/SelectedDataList/SelectedDataItem/index.js
+++ src/components/_shared/SelectedData/_parts/SelectedDataList/SelectedDataItem/index.js
@@ -37,7 +37,6 @@ const SelectedDataItem = ({
   );
   const isHighlighted = activePoint?.pointId === pointId;
   const [showContentMenu, setShowContentMenu] = useState(false);
-  const date = moment(timestamp).format('ddd, MMMM D, YYYY');
   const time = moment(timestamp).format('h:mma');
   const classes = classNames({
     [`${selectedDataItem}`]: true,
@@ -78,10 +77,9 @@ const SelectedDataItem = ({
   }, [isHighlighted]);
 
   return (
-    <li className={classes}>
+    <div className={classes}>
       <button type="button" onClick={handleClick} ref={itemRef}>
         <div className={selectedDataContent}>
-          <h6>{date}</h6>
           <ul>
             <li>
               <FontAwesomeIcon icon={faClock} /> {time}
@@ -107,7 +105,7 @@ const SelectedDataItem = ({
           closeAction={() => setShowContentMenu(false)}
         />
       )}
-    </li>
+    </div>
   );
 };
 
