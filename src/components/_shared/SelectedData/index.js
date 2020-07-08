77536bab8dfca97faf6909ea6158c4aa0cea6b7e
David Christian Jones <david@dcj.codes>
2020-07-08 15:42:49 -0400

    sidebar tweaks

--- src/components/_shared/SelectedData/index.js
+++ src/components/_shared/SelectedData/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
-import { selectedDataWrapper } from './SelectedData.module.scss';
-
 import SelectedDataList from 'components/_shared/SelectedData/SelectedDataList/SelectedDataList';
 
 const SelectedData = () => {
   // This is not longer used but im leaving it for the .stories
-  return (
-    <div className={selectedDataWrapper}>
-      <SelectedDataList />
-    </div>
-  );
+  return <SelectedDataList />;
 };
 
 export default SelectedData;
