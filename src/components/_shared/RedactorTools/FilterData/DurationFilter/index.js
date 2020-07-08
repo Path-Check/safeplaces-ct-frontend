390ed80c36838f1b85b2ac6d2640b3f325bead4e
Facundo Malgieri <facundo.malgieri@gmail.com>
2020-07-08 16:05:11 -0300

    [PLACES-421] Sidebar Redesign: Filter Updates (#264)
    * [PLACES-421] Sidebar Redesign: Filter Updates
    
    * scoping in css modules seems weird
    
    Co-authored-by: David Christian Jones <david@dcj.codes>

--- src/components/_shared/RedactorTools/FilterData/DurationFilter/index.js
+++ src/components/_shared/RedactorTools/FilterData/DurationFilter/index.js
@@ -1,50 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 import Slider from 'rc-slider';
 
 import {
+  durationText,
   durationFilter,
-  durationFilterSlider,
+  durationTitle,
   durationFilterSliderActive,
 } from './DurationFilter.module.scss';
-import Checkbox from 'components/_shared/ControlledCheckbox/Checkbox';
 
-const DurationFilter = ({
-  duration,
-  setDuration,
-  times,
-  checked,
-  setChecked,
-}) => {
+const DurationFilter = ({ duration, setDuration, times }) => {
   const handleChange = value => {
     setDuration(times[value]);
   };
 
-  const sliderClasses = classNames({
-    [`${durationFilterSlider}`]: true,
-    [`${durationFilterSliderActive}`]: checked,
-  });
-
   return (
     <div className={durationFilter}>
-      <Checkbox
-        onChange={setChecked}
-        label="Hide data less than duration"
-        id="durationFilter"
-        align="left"
-        isChecked={checked}
-      />
-      <div className={sliderClasses}>
+      <h3 className={durationTitle}>Duration</h3>
+      <div className={durationFilterSliderActive}>
+        <div className={durationText}>{duration} mins.</div>
         <Slider
           min={0}
           max={times.length - 1}
           step={1}
           onChange={handleChange}
-          disabled={!checked}
         />
-        <span>{duration} mins.</span>
       </div>
     </div>
   );
