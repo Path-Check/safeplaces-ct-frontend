75cd74d478e500aaf191413a07d02f7ab8ccf6c1
Facundo Malgieri <facundo.malgieri@gmail.com>
2020-07-27 17:49:47 -0300

    [PLACES-533] Members section UI (#317)

--- src/components/_shared/RedactorTools/Filters/FilterData/FilterData.stories.js
+++ src/components/_shared/RedactorTools/Filters/FilterData/FilterData.stories.js
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-import FilterData from './';
+// import FilterData from './';
 
-import SidebarWrapper from 'components/_shared/Sidebar/SidebarWrapper';
-import DurationFilter from 'components/_shared/RedactorTools/FilterData/DurationFilter';
+// import SidebarWrapper from 'components/_shared/Sidebar/SidebarWrapper';
+// import DurationFilter from 'components/_shared/RedactorTools/FilterData/DurationFilter';
 
-export default {
-  title: 'Filter Data',
-};
+// export default {
+//   title: 'Filter Data',
+// };
 
-export const Default = () => (
-  <SidebarWrapper>
-    <FilterData>
-      <DurationFilter />
-    </FilterData>
-  </SidebarWrapper>
-);
+// export const Default = () => (
+//   <SidebarWrapper>
+//     <FilterData>
+//       <DurationFilter />
+//     </FilterData>
+//   </SidebarWrapper>
+// );
