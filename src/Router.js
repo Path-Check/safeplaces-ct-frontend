2375b100567e045d583e19a74197a3e810656e8f
David Christian Jones <david@dcj.codes>
2020-07-29 11:25:40 -0400

    [PLACES-485] Reset Password (UI) (#323)
    * include formwrapper
    
    * Add reset UI
    
    * further reset password progress
    
    * add load state to button // improve resetting state
    
    * cta text
    
    * Add reset screen with initial validations
    
    * hook
    
    * Reset Password initial UI dev
    
    * fix indicator logic
    
    * cherry pie
    
    * use passinput on login

--- src/Router.js
+++ src/Router.js
@@ -10,35 +10,43 @@ import Authentication from 'views/Authentication';
 import Onboarding from 'views/Onboarding';
 import Settings from 'views/Settings';
 import ViewWrapper from 'views';
+import { useSelector } from 'react-redux';
+import applicationSelectors from 'ducks/application/selectors';
+import ModalSwitch from 'components/_global/Modal/ModalSwitch';
 
 function Router({ token }) {
   const location = useLocation();
+  const appStatus = useSelector(state => applicationSelectors.getStatus(state));
+  const mode = useSelector(state => applicationSelectors.getMode(state));
 
   return (
-    <Switch>
-      <Route path="/login/:action?" component={Authentication} />
-      {!token && (
+    <>
+      <Switch>
+        <Route path="/login/:action?" component={Authentication} />
+        {!token && (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { referrer: location.pathname || '/' },
+            }}
+          />
+        )}
+        <Route path="/onboarding/:action?" component={Onboarding} />
+        <Route path="/settings/:action?" component={Settings} />
+        <Route path="/trace" render={() => <ViewWrapper viewType="trace" />} />
+        <Route
+          path="/publish"
+          render={() => <ViewWrapper viewType="publish" />}
+        />
         <Redirect
+          from="/"
           to={{
-            pathname: '/login',
-            state: { referrer: location.pathname || '/' },
+            pathname: '/trace',
           }}
         />
-      )}
-      <Route path="/onboarding/:action?" component={Onboarding} />
-      <Route path="/settings/:action?" component={Settings} />
-      <Route path="/trace" render={() => <ViewWrapper viewType="trace" />} />
-      <Route
-        path="/publish"
-        render={() => <ViewWrapper viewType="publish" />}
-      />
-      <Redirect
-        from="/"
-        to={{
-          pathname: '/trace',
-        }}
-      />
-    </Switch>
+      </Switch>
+      <ModalSwitch mode={mode} status={appStatus} />
+    </>
   );
 }
 
