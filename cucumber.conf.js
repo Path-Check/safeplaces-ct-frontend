780221c0ebd9e1aa5f9922b0d6d602a14671584f
Deven Phillips <deven.phillips@redhat.com>
2020-08-15 11:58:02 -0400

    Add pretty reports to cucumber+nightwatch

--- cucumber.conf.js
+++ cucumber.conf.js
@@ -1,12 +1,28 @@
-const { setDefaultTimeout, AfterAll, BeforeAll } = require('cucumber');
+const fs = require('fs');
+const path = require('path');
+const { setDefaultTimeout, After, AfterAll, BeforeAll } = require('cucumber');
 const {
   createSession,
   closeSession,
   startWebDriver,
   stopWebDriver,
 } = require('nightwatch-api');
+const reporter = require('cucumber-html-reporter');
 
 setDefaultTimeout(60000);
+
+const attachedScreenshots = getScreenshots();
+
+function getScreenshots() {
+  try {
+    const folder = path.resolve(__dirname, 'screenshots');
+
+    const screenshots = fs.readdirSync(folder).map(file => path.resolve(folder, file));
+    return screenshots;
+  } catch (err) {
+    return [];
+  }
+}
 
 BeforeAll(async () => {
   await startWebDriver({});
@@ -16,4 +32,29 @@ BeforeAll(async () => {
 AfterAll(async () => {
   await closeSession();
   await stopWebDriver();
+
+  setTimeout(() => {
+    reporter.generate({
+      theme: 'bootstrap',
+      jsonFile: 'report/cucumber_report.json',
+      output: 'report/cucumber_report.html',
+      reportSuiteAsScenarios: true,
+      launchReport: true,
+      metadata: {
+        'App Version': '0.3.2',
+        'Test Environment': 'POC',
+      }
+    });
+  }, 0);
+});
+
+After(function() {
+  return Promise.all(
+    getScreenshots()
+      .filter(file => !attachedScreenshots.includes(file))
+      .map(file => {
+        attachedScreenshots.push(file);
+        return this.attach(fs.readFileSync(file), 'image/png');
+      }),
+  );
 });
