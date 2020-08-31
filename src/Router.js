84344d0d776e8d3ff79a2b44e9bbba3554ef6968
Lynn Gazis <52673158+LynnGazis@users.noreply.github.com>
2020-08-30 22:52:29 -0700

    Update Router.js

--- src/Router.js
+++ src/Router.js
@@ -13,27 +13,45 @@ import ViewWrapper from 'views';
 import { useSelector } from 'react-redux';
 import applicationSelectors from 'ducks/application/selectors';
 import ModalSwitch from 'components/_global/Modal/ModalSwitch';
+import {
+  AccessCode,
+  PersonalInformation,
+  PhoneNumber,
+  ResetPassword,
+} from './views/Registration';
 
 function Router({ token }) {
-  const location = useLocation();
+  const { pathname } = useLocation();
   const appStatus = useSelector(state => applicationSelectors.getStatus(state));
   const mode = useSelector(state => applicationSelectors.getMode(state));
+  const publicRoutes = [
+    '/registration',
+    '/phone',
+    '/verify',
+    '/reset-password',
+  ];
+  const isPublicRoute = publicRoutes.indexOf(pathname) > -1;
 
   return (
     <>
       <Switch>
         <Route path="/login/:action?" component={Authentication} />
-        {!token && (
+        {!token && !isPublicRoute && (
           <Redirect
             to={{
               pathname: '/login',
-              state: { referrer: location.pathname || '/' },
+              state: { referrer: pathname || '/' },
             }}
           />
         )}
         <Route path="/onboarding/:action?" component={Onboarding} />
+        <Route path="/reset-password" component={ResetPassword} />
+
         <Route path="/settings/:action?" component={Settings} />
         <Route path="/trace" render={() => <ViewWrapper viewType="trace" />} />
+        <Route path="/registration" component={PersonalInformation} />
+        <Route path="/phone" component={PhoneNumber} />
+        <Route path="/verify" component={AccessCode} />
         <Route
           path="/publish"
           render={() => <ViewWrapper viewType="publish" />}
